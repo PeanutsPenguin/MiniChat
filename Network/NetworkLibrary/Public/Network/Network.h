aa689c6b0220fa43f1404961cfd31ca5a6534de9
Malo-aka-PeanutsPenguin <m.sadoine@student.isartdigital.com>
2024-09-17 11:35:06 -0400

    LIB
    Creating the server class to open the network

--- Network/NetworkLibrary/Public/Network/Network.h
+++ Network/NetworkLibrary/Public/Network/Network.h
@@ -1,1 +1,13 @@
-void network();
+#pragma once
+
+class Server
+{
+public:
+	Server() = default;
+
+	void Start(int majorVersion, int minorVersion);
+
+	void Close();
+
+	~Server();
+};
--- Network/NetworkLibrary/Source/Network.cpp
+++ Network/NetworkLibrary/Source/Network.cpp
@@ -1,7 +1,29 @@
-#include "Network.h"
-#include "Platform.h"
+#include "network.h"
+#include "errorHandler.h"
 
-void network()
+
+void Server::Start(int majorVersion, int minorVersion)
 {
-	platform();
+	WSADATA data;
+	int resultCheck = WSAStartup(MAKEWORD(majorVersion, minorVersion), &data);
+	if (resultCheck)
+	{
+		errorHandler::reportWindowsError(TEXT("WSAStartup error :"), resultCheck);
+	}
+
+	if (LOBYTE(data.wVersion) == minorVersion && HIBYTE(data.wVersion) == majorVersion)
+		errorHandler::consolPrint(TEXT("Server is open\n"));
+
+	else
+		errorHandler::consolPrint(TEXT("Server couldn't open\n"));
+}
+
+void Server::Close()
+{
+	WSACleanup();
+}
+
+Server::~Server()
+{
+	WSACleanup();
 }
