64a55b6eccf6b24d71f9b850f74a508748c7e1e8
Malo-aka-PeanutsPenguin <m.sadoine@student.isartdigital.com>
2024-09-23 09:51:13 -0400

    CLIENT
    include the right files

--- Network/NetworkLibrary/Public/Network/client.h
+++ Network/NetworkLibrary/Public/Network/client.h
@@ -1,17 +1,26 @@
 #pragma once 
 
+#include <vector>
+#include <Windows.h>
+#include <sstream>
+
+struct pollfd;
+
 class client
 {
+public:
 	client();
 
 	void CreateAndConnect(const char* ipAdress, const char* port);
 
 	void createEvent();
+
 	void sendMessaage(const char* msg, int length);
 
 	void receivemessage();
 
 	HANDLE getEvent();
+
 	~client();
 
 private:
