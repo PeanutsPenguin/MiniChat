f6f484a28851f2319826a8b448e2392dc041533c
Malo-aka-PeanutsPenguin <m.sadoine@student.isartdigital.com>
2024-09-27 11:27:20 -0400

    SEREVER LIB
    Function to disconnect all the clients and shutdown the socket

--- Network/NetworkLibrary/Source/server.cpp
+++ Network/NetworkLibrary/Source/server.cpp
@@ -208,12 +208,32 @@ void Server::newConnectionMsg(uint64_t* newfd, char* nameBuffer)
 }
 }
 
-						closesocket(this->fds[i].fd);
+bool Server::clearServer(char* buf)
+{
+	std::string msg = buf;
+	std::string check = "/closeServer";
+	if (buf == check)
+	{
+		for (int i = 2; i < this->fds.size(); i++)
+		{
+			int iResult = shutdown(this->fds[i].fd, SD_SEND);
 
-						this->removefds(i);
+
+			if (iResult == SOCKET_ERROR)
+				errorHandler::reportWindowsError("shutdown failed\n", WSAGetLastError());
+
+
+						closesocket(this->fds[i].fd);
+		}
+
+		closesocket(this->fds[0].fd);
+		closesocket(this->fds[1].fd);
+
+		return true;
 					}
 
-					else 
+	return false;
+}
 					{ 
 						buf[nbytes - (nbytes == sizeof buf ? 1 : 0)] = '\0';
 						errorHandler::consolPrint(buf);
